8ee5775c9d2e82f5dd80a0e3f57dc92c5eabb94a
satyam4853 <vaishnavsatyam4853@gmail.com>
2026-05-04 18:27:15 +0530

    backend and dblocking files added

---
+++ backend-setup.tf
@@ -0,0 +1,53 @@
+resource "aws_s3_bucket" "backend_bucket" {
+  bucket = "my-terraform-backend-bucket"
+
+
+  tags = {
+    Name        = "Terraform Backend Bucket"
+    Environment = "Production"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "backend_bucket_public_access_block" {
+  bucket = aws_s3_bucket.backend_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "backend_bucket_versioning" {
+  bucket = aws_s3_bucket.backend_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "backend_bucket_encryption" {
+  bucket = aws_s3_bucket.backend_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "backend_lock_table" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "Production"
+  }
+}
+
